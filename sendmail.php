2920b20afbf07cf3f503baaee5e2e4715f83fbf8
kumarborkar22 <borkarkumar22@gmail.com>
2026-05-18 17:59:07 +0530

    mail

---
+++ sendmail.php
@@ -0,0 +1,22 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $to = "your@email.com"; // Change to your email address
+    $name = strip_tags(trim($_POST["name"]));
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $subject = strip_tags(trim($_POST["subject"]));
+    $message = trim($_POST["message"]);
+
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n";
+    $email_content .= "Subject: $subject\n";
+    $email_content .= "Message:\n$message\n";
+
+    $headers = "From: $name <$email>";
+
+    if (mail($to, $subject, $email_content, $headers)) {
+        echo "success";
+    } else {
+        echo "error";
+    }
+}
+?>
